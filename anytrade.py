6c0c31ee10a01dc4fe30f1bd15f5aff838aa9401
claytantor <claytantor@gmail.com>
2021-08-31 10:04:29 -0700

    added btc data

--- anytrade.py
+++ anytrade.py
@@ -22,6 +22,9 @@ from stable_baselines3.common.results_plotter import load_results, ts2xy, plot_r
 from stable_baselines3.common.noise import NormalActionNoise
 from stable_baselines3.common.callbacks import BaseCallback
 
+
+
+
 def load_dataset(name, index_name):
     base_dir = os.path.dirname(os.path.abspath(__file__))
     path = os.path.join(base_dir, 'data', name + '.csv')
@@ -48,60 +51,16 @@ def linear_schedule(initial_value: float) -> Callable[[float], float]:
     return func
 
 
-# class SaveOnBestTrainingRewardCallback(BaseCallback):
-#     """
-#     Callback for saving a model (the check is done every ``check_freq`` steps)
-#     based on the training reward (in practice, we recommend using ``EvalCallback``).
-
-#     :param check_freq: (int)
-#     :param log_dir: (str) Path to the folder where the model will be saved.
-#       It must contains the file created by the ``Monitor`` wrapper.
-#     :param verbose: (int)
-#     """
-#     def __init__(self, check_freq: int, log_dir: str, verbose=1):
-#         super(SaveOnBestTrainingRewardCallback, self).__init__(verbose)
-#         self.check_freq = check_freq
-#         self.log_dir = log_dir
-#         self.save_path = os.path.join(log_dir, 'best_model')
-#         self.best_mean_reward = -np.inf
-
-#     def _init_callback(self) -> None:
-#         # Create folder if needed
-#         if self.save_path is not None:
-#             os.makedirs(self.save_path, exist_ok=True)
-
-#     def _on_step(self) -> bool:
-        
-#         if self.n_calls % self.check_freq == 0:
-          
-#           # Retrieve training reward
-#           x, y = ts2xy(load_results(self.log_dir), 'timesteps')
-#           print("check best",x)  
-#           if len(x) > 0:
-#               # Mean training reward over the last 100 episodes
-#               mean_reward = np.mean(y[-100:])
-#               if self.verbose > 0:
-#                 print("Num timesteps: {}".format(self.num_timesteps))
-#                 print("Best mean reward: {:.2f} - Last mean reward per episode: {:.2f}".format(self.best_mean_reward, mean_reward))
-
-#               # New best model, you could save the agent here
-#               if mean_reward > self.best_mean_reward:
-#                   self.best_mean_reward = mean_reward
-#                   # Example for saving best model
-#                   if self.verbose > 0:
-#                     print("Saving new best model to {}".format(self.save_path))
-#                   self.model.save(self.save_path)
-
-#         return True
-
-
 def main(argv):
 
     log_path = "./log/sb3_log/"
     new_logger = configure(log_path, ["stdout", "csv", "tensorboard"])
     
-
-    df = gym_anytrading.datasets.STOCKS_GOOGL.copy()
+    # Load Stocks datasets
+    # data/COINS_USD_BTC.csv
+    COINS_USD_BTC = load_dataset('COINS_USD_BTC', 'Date')
+    df = COINS_USD_BTC.copy()
+    # df = gym_anytrading.datasets.STOCKS_GOOGL.copy()
 
     window_size = 20
     start_index = window_size
